2ec6fd79d9eaf7b7f9e79c2fcd11bb3976380e80
Angus Chang <angus@heysparkbox.com>
2021-08-18 09:46:45 -0400

    feat: add rouge class
    FSA21V2-88

--- src/rogue/index.js
+++ src/rogue/index.js
@@ -1,10 +1,23 @@
 import Character from '../character';
 
-class Rogue {
+class Rogue extends Character {
+  constructor(name) {
+    super(name);
+    this.health = 50;
+    this.numberOfAttacks = 0;
+  }
+
   talk() {
+    return `hello. my name is ${this.name.toLowerCase()}.`;
   }
 
   attack() {
+    this.numberOfAttacks++;
+    if (this.numberOfAttacks % 5 === 0) {
+      this.health -= 5;
+      return 1;
+    }
+    return 5;
   }
 }
 
