5fe83e7f93f12642b99db9f4ceee495be2494eed
Angus Chang <angus@heysparkbox.com>
2021-08-18 09:28:12 -0400

    feat: add wizard class
    the wizard class extends the character class and
    has unique talk and attack behavior. The attack behavior
    changes based on the number of attacks, so the wizard has
    a variable which keep tracks of that state.
    
    FSA21V2-88

--- src/wizard/index.js
+++ src/wizard/index.js
@@ -1,10 +1,24 @@
 import Character from '../character';
 
-class Wizard {
+class Wizard extends Character {
+  constructor(name){
+    super(name)
+    this.health = 50;
+    this.numberOfAttacks = 0;
+  }
+
   talk() {
+    return `Hello! My name is ${this.name}.`.toUpperCase()
   }
 
   attack() {
+    this.numberOfAttacks++ 
+    if (this.numberOfAttacks > 3) {
+      this.health -= 10;
+      return 1
+    } else {
+      return 2
+    }
   }
 }
 
